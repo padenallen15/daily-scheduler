5bc91f5422ad758effdaa52d654630776d62ab6b
Paden Allen <padenallen15@yahoo.com>
2021-01-10 20:21:14 -0600

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# daily-scheduler-3
+# daily-scheduler
+
+## Purpose
+A website to organize your daily activities.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+* Jquery
+
+## Website
+https://padenallen15.github.io/daily-scheduler/
+
+## Screenshot
+![Alt text](./assets/images/demo.png)
+
+## Contribution
+Made with ❤️ by [Paden Allen]
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,27 +1,22 @@
-var tasks = JSON.parse(localStorage.getItem('schedule')) || [];
-
-var eightAmTask = "";
-
-
+// Get current date and time
 var date = new Date();
 $('#currentDay').text(date);
-
 var time = date.getHours();
-var time = 13;
-console.log(time);
 
-
+// Sets color scheme for tasks depending on current time
 for(var num = 8; num < 18; num++) {
+    // present
     if (num < time) {
         $('#'+num+'Text').css("background-color", "#d3d3d3");
     }
+    // current
     else if (num === time) {
         $('#'+num+'Text').css("background-color", "#ff6961");
     }
+    // future
     else {
         $('#'+num+'Text').css("background-color", "#77dd77");
     }
-    console.log(num);
 }
 
 // task text was clicked
@@ -29,27 +24,33 @@ $(".textarea").on("click", function() {
     // get current text of p element
     var text = $(this)
       .text()
+      console.log(text);
     // replace p element with a new textarea
     var textInput = $("<textarea>")
+        .attr("id", "8Text")
         .addClass("col-8")
         .addClass("textarea")
         .text(text);
+        console.log(textInput);
     $(this).replaceWith(textInput);
     // auto focus new element
     textInput.trigger("focus");
 });
 
+// when save button is clicked
 $(".saveBtn").on("click", function() {
+    // get value user entered, doesnt work :(
     var text = $(this)
         .closest(".textarea")
         .text();
-    // recreate p element
+        console.log(text);
+    // recreate p element, doesnt work
     var taskP = $("<p>")
         .addClass("col-8")
         .addClass("textarea")
         .text(text);
-    // replace textarea with p element
+    // replace textarea with p element, doesnt work
     $(this).closest(".textarea").replaceWith(taskP);
 })
 
-localStorage.setItem('schedule', JSON.stringify(tasks));
+//localStorage.clear();
